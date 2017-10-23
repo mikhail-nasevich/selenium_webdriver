e8694e77e5099b0b23e4f8f85beaa6ee7495d588
n-mikhail <n-mikhail@users.noreply.github.com>
2017-10-23 19:33:48 +0300

    java class update

--- WebDriver_Part1/src/test/java/webriver_1/SayHello.java
+++ WebDriver_Part1/src/test/java/webriver_1/SayHello.java
@@ -3,7 +3,7 @@ package webriver_1;
 
 public class SayHello {
 
-	public void test() {
+	public static void main(String[] arg) {
 			System.out.println(" - > Say Hello and Be Ready to Hard Work");
 			System.out.println(" - > Hello everyone. I am ready");
 		}
